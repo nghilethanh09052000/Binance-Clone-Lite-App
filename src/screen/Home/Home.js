6dcc66ec2d1e3149abe731336a1956df555228ca
ThanhNghi <nghilt19411@gmail.com>
2022-04-11 20:26:44 +0700

    create component

---
+++ src/screen/Home/Home.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+const Home = () => {
+    return (
+        <View>
+            <Text>Hello</Text>
+        </View>
+      );
+}
+ 
+export default Home;
